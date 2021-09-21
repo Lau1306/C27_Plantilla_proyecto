cc8d7b4d532ce10d53bcfb24e9d6d4aa522e988a
Lau1306 <70720577+Lau1306@users.noreply.github.com>
2021-09-21 00:29:07 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 const Render = Matter.Render;
 const Constraint = Matter.Constraint;
-var bob1,bob2,bob3, bob4,bob5, bob6;
+var bob1,bob2,bob3, bob4,bob5;
 var rope1,rope2,rope3, rope4,rope5;
 var world;
 
